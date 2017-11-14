7503304caa99c4ffcbde84941877a00ecb657888
h-shvedko <hennadii.shvedko@copernicus.org>
2017-11-14 11:38:25 +0100

    ADDED cards view

--- src/main/java/com/cards/shvedko/Controller/CardController.java
+++ src/main/java/com/cards/shvedko/Controller/CardController.java
@@ -5,6 +5,7 @@ import com.cards.shvedko.Model.Decks;
 import com.cards.shvedko.Model.DecksValues;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -41,17 +42,78 @@ public class CardController extends A_Controller {
     @FXML
     public ImageView translatedExampleSound;
 
+    private int numberOfElement = 0;
+    public ObservableList<Cards> cardsTable = FXCollections.observableArrayList();
+
     public void initialize(URL location, ResourceBundle resources) {
 
-        ObservableList<Cards> cardsTable = FXCollections.observableArrayList();
         List deckValues = ((Decks)A_Controller.globalUserData).getDecksValues();
 
         for(Object deckValue: deckValues){
             cardsTable.add(((DecksValues)deckValue).getCards());
         }
+        checkNumberOfWords();
+        setValuesOfCard();
+    }
 
-        for(Object card: cardsTable){
-            word.setText(((Cards)card).getName());
+    private void setValuesOfCard() {
+        word.setText(cardsTable.get(getNumberOfElement()).getName());
+        translatedWord.setText(cardsTable.get(getNumberOfElement()).getForeignName());
+        example.setText(cardsTable.get(getNumberOfElement()).getExample());
+        translatedExample.setText(cardsTable.get(getNumberOfElement()).getForeignExample());
+    }
+
+    public int getNumberOfElement() {
+        return numberOfElement;
+    }
+
+    public void setNumberOfElement(int numberOfElement) {
+        this.numberOfElement = numberOfElement;
+    }
+
+    public void handlePreviousButton(ActionEvent actionEvent) {
+        int numberOfWord = getNumberOfElement();
+
+        if(numberOfWord > 0){
+            setNumberOfElement(--numberOfWord);
+            setValuesOfCard();
         }
+
+        checkNumberOfWords();
+    }
+
+    public void handleNextButton(ActionEvent actionEvent) {
+        int numberOfWord = getNumberOfElement();
+
+        if(numberOfWord < cardsTable.size() - 1){
+            setNumberOfElement(++numberOfWord);
+            setValuesOfCard();
+        }
+
+        checkNumberOfWords();
+    }
+
+    private void checkNumberOfWords() {
+        int numberOfWord = getNumberOfElement();
+
+        if(numberOfWord == 0){
+            previous.setDisable(true);
+        } else {
+            previous.setDisable(false);
+        }
+
+        if(numberOfWord == cardsTable.size() - 1){
+            next.setDisable(true);
+        } else {
+            next.setDisable(false);
+        }
+    }
+
+    public void handleSettingsButton(ActionEvent actionEvent) {
+
+    }
+
+    public void handleEditButton(ActionEvent actionEvent) {
+
     }
 }
