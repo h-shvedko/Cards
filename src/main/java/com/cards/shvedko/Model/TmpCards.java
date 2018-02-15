eda0140446a86cded0a643e93cbbdd0620f08fcf
h-shvedko <hennadii.shvedko@copernicus.org>
2018-02-15 09:52:04 +0100

    ADDED table for temporary storing data which is importing from internal sources

---
+++ src/main/java/com/cards/shvedko/Model/TmpCards.java
@@ -0,0 +1,224 @@
+package com.cards.shvedko.Model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TMP_CARDS")
+public class TmpCards extends A_Models implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true, nullable = false)
+    private int id;
+
+    @NotNull(message = "Native word value of Card can't be empty!")
+    @Size(min = 2, message = "Native word value of Card can't be less then 2 symbols!")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull(message = "Foreign word value of Card can't be empty!")
+    @Size(min = 2, message = "Foreign word value of Card can't be less then 2 symbols!")
+    @Column(name = "foreign_name", nullable = false)
+    private String foreignName;
+
+    @Column(name = "foreign_name_infinitive")
+    private String foreignNameInfinitive;
+
+    @Column(name = "foreign_name_preteritum")
+    private String foreignNamePreteritum;
+
+    @Column(name = "foreign_name_perfect")
+    private String foreignNamePerfect;
+
+    @Column(name = "example")
+    private String example;
+
+    @Column(name = "foreign_example")
+    private String foreignExample;
+
+    @ManyToOne(optional = false) //(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private CardCategories category;
+
+    @ManyToOne(optional = false) //(fetch = FetchType.LAZY)
+    @JoinColumn(name = "type_id", referencedColumnName = "id")
+    private CardTypes type;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "preposition_akk", referencedColumnName = "id")
+    private CardsPrepositionAkkusativ prepositionAkk;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "preposition_dat", referencedColumnName = "id")
+    private CardsPrepositionDativ prepositionDativ;
+
+    @Column(name = "kind_of_noun", length = 1)
+    private int kindOfNoun;
+
+    @Column(name = "plural_endung")
+    private String pluralEndung;
+
+    @Column(name = "is_regular_verb", length = 1)
+    private int isRegularVerb;
+
+    @Column(name = "is_trembare_prefix_verb", length = 1)
+    private int isTrembarePrefixVerb;
+
+    @Column(name = "is_perfect_with_haben", length = 1)
+    private int isPerfectWithHaben;
+
+    @Column(name = "is_reflexiv_verb", length = 1)
+    private int isReflexiveVerb;
+
+    @Column(name = "preposition_gen")
+    private String prepositionGen;
+
+    @SuppressWarnings("UnusedDeclaration")
+    public TmpCards() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getForeignName() {
+        return foreignName;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setForeignName(String value) {
+        this.foreignName = value;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public String getExample() {
+        return example;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public int getKindOfNoun() {
+        return kindOfNoun;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setKindOfNoun(int kindOfNoun) {
+        this.kindOfNoun = kindOfNoun;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public int getIsRegularVerb() {
+        return isRegularVerb;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setIsRegularVerb(int isRegularVerb) {
+        this.isRegularVerb = isRegularVerb;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public int getIsTrembarePrefixVerb() {
+        return isTrembarePrefixVerb;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setIsTrembarePrefixVerb(int isTrembarePrefixVerb) {
+        this.isTrembarePrefixVerb = isTrembarePrefixVerb;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public int getIsPerfectWithHaben() {
+        return isPerfectWithHaben;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setIsPerfectWithHaben(int isPerfectWithHaben) {
+        this.isPerfectWithHaben = isPerfectWithHaben;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public int getIsReflexiveVerb() {
+        return isReflexiveVerb;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setIsReflexiveVerb(int isReflexiveVerb) {
+        this.isReflexiveVerb = isReflexiveVerb;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public String getPrepositionGen() {
+        return prepositionGen;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public void setPrepositionGen(String prepositionGen) {
+        this.prepositionGen = prepositionGen;
+    }
+
+    public CardCategories getCategory() {
+        return category;
+    }
+
+    public void setCategory(CardCategories category) {
+        this.category = category;
+    }
+
+    public CardTypes getType() {
+        return type;
+    }
+
+    public void setType(CardTypes type) {
+        this.type = type;
+    }
+
+    public String getForeignExample() {
+        return foreignExample;
+    }
+
+    public void setForeignExample(String foreignExample) {
+        this.foreignExample = foreignExample;
+    }
+
+    public String getPluralEndung() {
+        return pluralEndung;
+    }
+
+    public void setPluralEndung(String pluralEndung) {
+        this.pluralEndung = pluralEndung;
+    }
+
+    public CardsPrepositionDativ getPrepositionDativ() {
+        return prepositionDativ;
+    }
+
+    public void setPrepositionDativ(CardsPrepositionDativ prepositionDativ) {
+        this.prepositionDativ = prepositionDativ;
+    }
+
+    public CardsPrepositionAkkusativ getPrepositionAkk() {
+        return prepositionAkk;
+    }
+
+    public void setPrepositionAkk(CardsPrepositionAkkusativ prepositionAkk) {
+        this.prepositionAkk = prepositionAkk;
+    }
+}
---
+++ src/main/java/com/cards/shvedko/ModelDAO/TmpCardsDAO.java
@@ -0,0 +1,54 @@
+package com.cards.shvedko.ModelDAO;
+
+import com.cards.shvedko.Model.TmpCards;
+import org.hibernate.HibernateException;
+
+/**
+ * Created by hennadii.shvedko on 14/07/2017.
+ */
+public class TmpCardsDAO extends ModelsDAO {
+
+    public TmpCards tmpCards;
+
+    public TmpCardsDAO() {
+        super();
+        tmpCards = new TmpCards();
+    }
+
+    public TmpCardsDAO(int id) {
+        super();
+        tmpCards = (TmpCards) session.get(TmpCards.class, id);
+    }
+
+    public TmpCards getCardById(int id) throws HibernateException {
+        return (TmpCards) session.get(TmpCards.class, id);
+    }
+
+//    public Cards getCardById(int id) throws HibernateException{
+//        Criteria criteria = session.createCriteria(Cards.class);
+//        Cards tmpCardsModel = (Cards) criteria.add(Restrictions.eq("id", id)).uniqueResult();
+//        return tmpCardsModel;
+//    }
+
+    public boolean save() throws Exception {
+        if (errorMsg== null || errorMsg.equals("")) {
+            session.persist(tmpCards);
+            transaction.commit();
+            session.close();
+        } else{
+            return false;
+        }
+        return true;
+    }
+
+    public boolean saveOrUpdate() {
+        if (errorMsg == null || errorMsg.equals("")) {
+            session.save(tmpCards);
+            transaction.commit();
+            session.close();
+        } else {
+            return false;
+        }
+        return true;
+    }
+}
