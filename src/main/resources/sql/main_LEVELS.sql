013b1a8e09d60ab8c0b3eac397b0cbadbf763ed2
h-shvedko <hennadii.shvedko@copernicus.org>
2018-12-11 16:32:52 +0100

    FIXED: #97 Cards-97 Экран "Создание колоды" Создается пустышка при выборе всех параметров, исключая кнопки "...

--- src/main/resources/sql/main_LEVELS.sql
+++ src/main/resources/sql/main_LEVELS.sql
@@ -1,7 +1,7 @@
-INSERT INTO LEVELS (id, is_visible, name) VALUES (0, 1, 'All');
-INSERT INTO LEVELS (id, is_visible, name) VALUES (1, 1, 'A1');
-INSERT INTO LEVELS (id, is_visible, name) VALUES (2, 1, 'A2');
-INSERT INTO LEVELS (id, is_visible, name) VALUES (3, 1, 'B1');
-INSERT INTO LEVELS (id, is_visible, name) VALUES (4, 1, 'B2');
-INSERT INTO LEVELS (id, is_visible, name) VALUES (5, 1, 'C1');
-INSERT INTO LEVELS (id, is_visible, name) VALUES (6, 1, 'C2');
+INSERT INTO LEVELS (id, is_visible, name) VALUES (1, 1, 'All');
+INSERT INTO LEVELS (id, is_visible, name) VALUES (2, 2, 'A1');
+INSERT INTO LEVELS (id, is_visible, name) VALUES (3, 3, 'A2');
+INSERT INTO LEVELS (id, is_visible, name) VALUES (4, 4, 'B1');
+INSERT INTO LEVELS (id, is_visible, name) VALUES (5, 5, 'B2');
+INSERT INTO LEVELS (id, is_visible, name) VALUES (6, 5, 'C1');
+INSERT INTO LEVELS (id, is_visible, name) VALUES (7, 6, 'C2');
